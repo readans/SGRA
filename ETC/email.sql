fd70a5b10d73096a2d1e230aaf4e00488d14cd31
Dylan Steven Ariza Rueda <dylan.ariza@novabase.com.co>
2023-04-21 10:04:25 -0500

    nueva estructura del proyecto y primer trigger

---
+++ ETC/email.sql
@@ -0,0 +1,28 @@
+sp_configure 'show advanced options', 1;  
+GO  
+RECONFIGURE;  
+GO
+sp_configure 'Database Mail XPs', 1;  
+GO  
+RECONFIGURE;
+GO
+-- Create a Database Mail account
+EXECUTE msdb.dbo.sysmail_add_account_sp
+    @account_name = 'SQL Agent Account',
+    @description = 'Mail account for Azure SQL Managed Instance SQL Agent system.',
+    @email_address = '$(loginEmail)',
+    @display_name = 'SQL Agent Account',
+    @mailserver_name = '$(mailserver)' ,
+    @username = '$(loginEmail)' ,
+    @password = '$(password)';
+-- Create a Database Mail profile
+EXECUTE msdb.dbo.sysmail_add_profile_sp
+    @profile_name = 'AzureManagedInstance_dbmail_profile',
+    @description = 'E-mail profile used for messages sent by Managed Instance SQL Agent.';
+
+-- Add the account to the profile
+EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
+    @profile_name = 'AzureManagedInstance_dbmail_profile',
+    @account_name = 'SQL Agent Account',
+    @sequence_number = 1;
+GO
